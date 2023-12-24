127f9402c07bc819c2705a8337e4cfe284244d27
Roman <pliskarv@yandex.ru>
2023-12-24 16:02:20 +0900

    4

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. Напишите программу, которая принимает на вход трёхзначное
+//    число и возводит вторую цифру этого числа в степень, равную
+//    третьей цифре.
+
+
+int a = int.Parse(Console.ReadLine()!);
+
+int SecondNum = a / 10 %10;
+
+int ThirdNumber = a % 10;
+
+Console.WriteLine (Math.Pow(SecondNum,ThirdNumber));
