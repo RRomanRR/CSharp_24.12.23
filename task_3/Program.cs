d8e6106ff65d9d213b341e327f02a633e42f72e8
Roman <pliskarv@yandex.ru>
2023-12-25 11:07:55 +0900

    проблема с сохранением

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -1,6 +1,16 @@
-﻿
+﻿// 3. Напишите программу, которая будет принимать на вход два
+//    числа и выводить, является ли второе число кратным первому.
+//    Если второе число некратно первому, то программа выводит
+//    остаток от деления.
+
+
 int a = int.Parse(Console.ReadLine()!);
 int b = int.Parse(Console.ReadLine()!);
 if (a%b==0){Console.WriteLine("да")};
 else{Console.WriteLine("нет"+a%b)};
+
+int a=int.Parse(Console.ReadLine()!);
+int b=int.Parse(Console.ReadLine()!);
+if(a%b==0){Console.WriteLine("да");}
+else{Console.WriteLine("нет "+a%b);}
 
---
+++ task_4/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+
+int a=int.Parse(Console.ReadLine()!);
+
+if(a<100){Console.WriteLine("нет");}
+else{Console.WriteLine("да "+a/100%10);}
+
